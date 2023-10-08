f3987879372da7f85f3527cc60a175ab228ef6cf
Mariel Anabella <gutierrezmariel2605@gmail.com>
2023-10-08 01:18:52 -0300

    armando formulario tipo login

---
+++ Formulario/FormLogin.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Formulario
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            btnSalir = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LimeGreen;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(27, 186);
+            button1.Name = "button1";
+            button1.Size = new Size(312, 45);
+            button1.TabIndex = 0;
+            button1.Text = "INGRESAR";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(264, 284);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 1;
+            btnSalir.Text = "SALIR";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(27, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(153, 30);
+            label1.TabIndex = 2;
+            label1.Text = "INICIO SESION";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(27, 96);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(312, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(27, 145);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(312, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Correo:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 127);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Contraseña:";
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(366, 319);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(btnSalir);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormLogin";
+            Text = "Form1";
+            Load += FormLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button btnSalir;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Formulario/FormLogin.cs
@@ -0,0 +1,20 @@
+namespace Formulario
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
